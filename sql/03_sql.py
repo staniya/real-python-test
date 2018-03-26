8f6768a4a93f9344107a6ffcb86f765e3caf4f3c
staniya <staniya@thacher.org>
2018-03-26 11:53:20 +0800

    sql-database 4

--- sql/03_sql.py
+++ sql/03_sql.py
@@ -4,23 +4,29 @@
 # import the sqlite3 library
 import sqlite3
 
-
 # create connection object
-conn = sqlite3.connect("new.db")
+with sqlite3.connect("new.db") as connection:
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
 
+    # insert the multiple records using a tuple
+    cities = [
+        ('Boston', 'MA', 600000),
+        ('Chicago', 'IL', 2700000),
+        ('Houston', 'TX', 2100000),
+        ('Phoenix', 'AZ', 1500000)
+    ]
 
-# get a cursor object used to execute SQL commands
-cursor = conn.cursor()
+    # insert data into table
+    c.executemany('INSERT INTO population VALUES(?,?,?)', cities)
+    # # insert data
+    # c.execute("INSERT INTO population "
+    #           "VALUES('New York City', 'NY', 8400000)")
+    # c.execute("INSERT INTO population "
+    #           "VALUES('San Francisco', 'CA', 800000)")
 
-
-# insert data
-cursor.execute("INSERT INTO population "
-               "VALUES('New York City', 'NY', 8400000)")
-cursor.execute("INSERT INTO population "
-               "VALUES('San Francisco', 'CA', 800000)")
-
+# You need the following if it wasn't a with statement
 # commit the changes
-conn.commit()
-
+#   conn.commit()
 # close the database connection
-conn.close()
+#   conn.close()
